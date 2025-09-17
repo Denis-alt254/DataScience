b2e67ff313abb97f70bc5c6291f9fa17282f159e
Denis-alt254 <deniskipkurui2024@gmail.com>
2025-09-17 12:01:59 +0300

    Checking an even number

---
+++ Even.py
@@ -0,0 +1,9 @@
+num = int(input("Enter the number: "))
+result = num%2
+print("Result: ", result)
+print("*****************")
+
+if (result == 0):
+    print("Even number")
+else:
+    print("Odd number")
