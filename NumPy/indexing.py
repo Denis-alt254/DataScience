aa887a1fb343ed1b78ee92cb0a636a2eafb073d0
Denis-alt254 <deniskipkurui2024@gmail.com>
2025-09-30 14:02:42 +0300

    accessing array elements using indexes

---
+++ NumPy/indexing.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+# 1-D array
+
+arr = np.array([1,2,3,4])
+
+# accessing the elements in an array using indexes
+
+print(arr[0])
+
+# 2-D array
+
+arr2 = np.array([[1,2,3,4], [5,6,7,8]])
+
+print(arr2[1,0])
+
+# 3-D array
+
+arr3 = np.array([[[1,2,3], [5,6,7]], [[2,3,4], [5,6,7]]])
+
+print(arr3[0,0,0])
